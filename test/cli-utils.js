3f7a5b6b8f78baf879714c0af3a6e225ba891c6e
Jeroen Engels <jfm.engels@gmail.com>
2016-04-05 22:08:30 +0200

    Add CLI to help select/run codemods

---
+++ test/cli-utils.js
@@ -0,0 +1,94 @@
+import test from 'ava';
+import {sortByVersion, getVersions, selectScripts} from '../cli-utils';
+
+test('sortByVersion', t => {
+	const array = [
+		{version: '1.0.1'},
+		{version: '88.0.1'},
+		{version: '0.0.1'},
+		{version: '1.0.0'},
+		{version: '1.10.0'},
+		{version: '1.2.0'}
+	];
+
+	t.same(array.sort(sortByVersion), [
+		{version: '0.0.1'},
+		{version: '1.0.0'},
+		{version: '1.0.1'},
+		{version: '1.2.0'},
+		{version: '1.10.0'},
+		{version: '88.0.1'}
+	]);
+});
+
+test('getVersions', t => {
+	const array = [
+		{version: '1.0.1'},
+		{version: '88.0.1'},
+		{version: '0.0.1'},
+		{version: '1.0.0'},
+		{version: '1.10.0'},
+		{version: '1.2.0'}
+	];
+
+	t.same(getVersions(array), [{
+		name: 'older than 0.0.1',
+		value: '0.0.0'
+	},
+	'0.0.1',
+	'1.0.0',
+	'1.0.1',
+	'1.2.0',
+	'1.10.0',
+	'88.0.1',
+	{
+		name: 'latest',
+		value: '9999.9999.9999'
+	}]);
+});
+
+test('selectScripts', t => {
+	var codemods = [{
+		version: '0.14.0',
+		scripts: [
+			'lib/ok-to-truthy.js',
+			'lib/same-to-deep-equal.js'
+		]
+	}, {
+		version: '0.15.0',
+		scripts: [
+			'lib/script-0.15.0.js'
+		]
+	}, {
+		version: '1.0.0',
+		scripts: [
+			'lib/script-1.0.0.js'
+		]
+	}, {
+		version: '2.0.0',
+		scripts: [
+			'lib/script-2.0.0.js'
+		]
+	}];
+
+	t.same(selectScripts(codemods, '0.14.0', '0.14.0'), []);
+
+	t.same(selectScripts(codemods, '0.14.0', '1.0.0'), [
+		'lib/script-0.15.0.js',
+		'lib/script-1.0.0.js'
+	]);
+
+	t.same(selectScripts(codemods, '0.0.0', '0.15.0'), [
+		'lib/ok-to-truthy.js',
+		'lib/same-to-deep-equal.js',
+		'lib/script-0.15.0.js'
+	]);
+
+	t.same(selectScripts(codemods, '0.0.0', '9999.9999.9999'), [
+		'lib/ok-to-truthy.js',
+		'lib/same-to-deep-equal.js',
+		'lib/script-0.15.0.js',
+		'lib/script-1.0.0.js',
+		'lib/script-2.0.0.js'
+	]);
+});
