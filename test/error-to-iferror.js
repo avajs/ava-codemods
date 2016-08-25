b04f05210970c4b2f206a99911e23f5f617dd043
Jeroen Engels <jfm.engels@gmail.com>
2016-08-25 23:56:59 +0200

    Use `jscodeshift-ava-tester` to create codemod tests (#29)

--- test/error-to-iferror.js
+++ test/error-to-iferror.js
@@ -1,11 +1,9 @@
 import test from 'ava';
+import jscodeshift from 'jscodeshift';
+import testPlugin from 'jscodeshift-ava-tester';
 import plugin from '../lib/error-to-iferror';
-import {wrapPlugin} from './_helpers';
 
-const wrapped = wrapPlugin(plugin);
+const {testChanged, testUnchanged} = testPlugin(jscodeshift, test, plugin);
 
-function tester(t, input, expected) {
-	t.is(wrapped(input), expected);
-}
-
-test('error >> ifError', tester, 't.error(foo, bar)', 't.ifError(foo, bar)');
+testChanged('error >> ifError', 't.error(foo, bar)', 't.ifError(foo, bar)');
+testUnchanged('unchanged ifError', 't.ifError(foo, bar)');
--- test/same-to-deep-equal.js
+++ test/same-to-deep-equal.js
@@ -1,20 +1,25 @@
 import test from 'ava';
+import jscodeshift from 'jscodeshift';
+import testPlugin from 'jscodeshift-ava-tester';
 import plugin from '../lib/same-to-deep-equal';
-import {wrapPlugin} from './_helpers';
 
-const wrapped = wrapPlugin(plugin);
+const {testChanged, testUnchanged} = testPlugin(jscodeshift, test, plugin);
 
-function tester(t, input, expected) {
-	t.is(wrapped(input), expected);
-}
-
-test('same >> deepEqual', tester, 't.same(foo, bar)', 't.deepEqual(foo, bar)');
-test('notSame >> notDeepEqual', tester, 't.notSame(foo, bar)', 't.notDeepEqual(foo, bar)');
-test('full test expression with same >> deepEqual', tester, `
+testChanged('same >> deepEqual', 't.same(foo, bar)', 't.deepEqual(foo, bar)');
+testChanged('notSame >> notDeepEqual', 't.notSame(foo, bar)', 't.notDeepEqual(foo, bar)');
+testChanged('full test expression with same >> deepEqual', `
 test(t => {
 		t.same(fn.sync('1.tmp', {cwd: t.context.tmp}), [path.join(t.context.tmp, '1.tmp')]);
 });
 `, `
+test(t => {
+		t.deepEqual(fn.sync('1.tmp', {cwd: t.context.tmp}), [path.join(t.context.tmp, '1.tmp')]);
+});
+`);
+
+testUnchanged('unchanged deepEqual', 't.deepEqual(foo, bar)');
+testUnchanged('unchanged notDeepEqual', 't.notDeepEqual(foo, bar)');
+testUnchanged('unchanged full test expression with deepEqual', `
 test(t => {
 		t.deepEqual(fn.sync('1.tmp', {cwd: t.context.tmp}), [path.join(t.context.tmp, '1.tmp')]);
 });
